7111c89dec9d11f93716e8068b994f40d4e5b3f0
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-18 19:24:21 +0100

    Finished an estate for techies

--- src/pages/homepage/EForTechies.tsx
+++ src/pages/homepage/EForTechies.tsx
@@ -6,23 +6,23 @@ import { Button } from '../../components';
 const EForTechies = () => {
   return (
     <Fragment>
-      <section className='bg-e-for-techies w-full min-h-[630px] py-[275px] flex justify-center'>
-        <div className='inner-container'>
-          <div className='backdrop-blur-md bg-[#797979]/10 rounded-[20px] shadow-md py-[50px] px-10 grid gap-6 w-[65ch]'>
+      <section className='bg-e-for-techies w-full min-h-[630px] flex justify-center laptop:h-[850px]'>
+        <div className='inner-container flex items-center max-tablet:justify-center xltablet:justify-start'>
+          <div className='backdrop-blur-md bg-[#797979]/10 rounded-[20px] shadow-md py-[50px] px-10 grid gap-6 w-fit'>
             <header>
-              <h2 className='text-3xl leading-[44px] text-white font-bold'>
+              <h2 className='text-brandBlue5 text-3xl font-bold max-tablet:w-[12ch]'>
                 An Estate for Techies
               </h2>
             </header>
-            <p className='text-white text-2xl'>
-              Embrace a tech-savvy neighborhood, leaving behind discrimination
-              and misconceptions. Together, we can exchange innovative ideas and
+            <p className='text-brandBlue5 text-lg leading-7 w-[21ch] tablet:w-[30ch] tablet:text-xl tablet:leading-8 laptop:w-[35ch] laptop:text-2xl laptop:leading-9'>
+              Embrace a tech-savvy neighbor, leaving behind discrimination and
+              misconceptions. Together, we can exchange innovative ideas and
               foster a supportive community.
             </p>
             <Button
               variant='secondary'
               text='Rent Apartment'
-              className='text-black font-satoshi'
+              className='!text-black font-satoshi'
             />
           </div>
         </div>
