7f7ae605d74997c4d941d443f87abe7a10874b49
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-19 05:49:53 +0100

    arranged 'what we offer' grid

--- src/components/block/WwoCard.tsx
+++ src/components/block/WwoCard.tsx
@@ -7,16 +7,21 @@ import ArrowRight from '../../assets/svg_icons/ArrowRight';
 type CardProps = {
   title: string;
   body: string;
+  className?: string;
 };
 
-const WwoCard: React.FC<CardProps> = ({ title, body }) => {
+const WwoCard: React.FC<CardProps> = ({ title, body, className }) => {
+  // declare classes common to all cards
+  const commonStyles =
+    'w-fit rounded-[20px] border-2 border-brandBlue5 py-9 px-6 grid grid-rows-[max-content,_1fr,_max-content] gap-6';
+
+  // let common styles be merged with classname styles
+  const styles = `${commonStyles} ${className}`;
   return (
-    <div className='rounded-[20px] border-2 border-brandBlue5 py-9 px-6 grid grid-rows-[min-content,_1fr,_min-content] gap-5'>
-      <h3 className='text-brandDeepGray100 font-medium'>
-        {title}
-      </h3>
-      <p className='text-brandDeepGray100'>{body}</p>
-      <div className='size-10 xllaptop:size-[70px] border rounded-full border-brandDeepGray2 text-brandDeepGray2 flex items-center justify-center overflow-hidden'>
+    <div className={styles}>
+      <h3 className='text-brandDeepGray100 font-medium'>{title}</h3>
+      <p className='text-brandDeepGray100 pb-5'>{body}</p>
+      <div className='size-10 xltablet:size-14 xllaptop:size-[70px] border rounded-full border-brandDeepGray2 text-brandDeepGray2 flex items-center justify-center overflow-hidden'>
         <ArrowRight className='size-9 xllaptop:size-14' strokeWidth={1.5} />
       </div>
     </div>
--- src/pages/homepage/WhatWeOffer.tsx
+++ src/pages/homepage/WhatWeOffer.tsx
@@ -12,27 +12,26 @@ const WhatWeOffer = () => {
             <h2 className='font-medium text-brandBlue100'>What we offer</h2>
           </header>
 
-          <div className='min-h-[500px] w-full pb-[50px]'>
-            <div className='w-full grid grid-cols-[repeat(auto-fill,_minmax(335px,_1fr))] gap-6 justify-center'>
-              <WwoCard
-                title='Standard Customized Apartments'
-                body='We have arranged ultimate tech-inspired living experience,
+          <div className='w-full grid justify-center grid-cols-[repeat(auto-fill,_minmax(335px,_1fr))] gap-6 '>
+            <WwoCard
+              title='Standard Customized Apartments'
+              body='We have arranged ultimate tech-inspired living experience,
                 customized apartments with furnished interiors and essential
                 equipment, including workstations, monitors, and ergonomic
                 furniture, ensuring an optimal environment for productivity and
                 innovation.'
-              />
+            />
 
-              <WwoCard
-                title='Platform to Buy and Sell neatly used gadgets'
-                body="Discover great deals on our platform where you can buy and sell neatly used gadgets at affordable prices. Whether you're looking to upgrade or declutter, find quality electronics that are in excellent condition. Shop smart and save on your favorite tech essentials today!"
-              />
+            <WwoCard
+              title='Platform to Buy and Sell neatly used gadgets'
+              body="Discover great deals on our platform where you can buy and sell neatly used gadgets at affordable prices. Whether you're looking to upgrade or declutter, find quality electronics that are in excellent condition. Shop smart and save on your favorite tech essentials today!"
+            />
 
-              <WwoCard
-                title='A Community for tech enthusiasts'
-                body='Stay connected with a vibrant community of tech enthusiasts through our Estate Networking platform. Join a diverse of individuals passionate about technology, innovation, and collaboration. Share insights, and foster connections with like-minded professionals in the tech world.'
-              />
-            </div>
+            <WwoCard
+              title='A Community for tech enthusiasts'
+              body='Stay connected with a vibrant community of tech enthusiasts through our Estate Networking platform. Join a diverse of individuals passionate about technology, innovation, and collaboration. Share insights, and foster connections with like-minded professionals in the tech world.'
+              className='min-[770px]:max-[1170px]:col-span-2 min-[770px]:max-[1170px]:max-w-[600px] justify-self-center'
+            />
           </div>
         </div>
       </section>
