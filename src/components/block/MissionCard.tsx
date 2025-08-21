2c2857e435fd6bc3ab11578cef8daa4563b6dce5
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-21 20:40:33 +0100

    minor update to missdionCard

--- src/components/block/MissionCard.tsx
+++ src/components/block/MissionCard.tsx
@@ -19,7 +19,7 @@ const MissionCard: React.FC<CardProps> = ({ title, desc, img, className }) => {
 
   return (
     <div className={styles}>
-      <div className='py-8 px-8 grid gap-8  '>
+      <div className='py-8 px-8 grid gap-8 grid-rows-[1fr,_fit-content]'>
         
         <div className='h-fit grid gap-3'>
           <h3 className='font-bold'>{title}</h3>
