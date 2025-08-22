c46dc6429f3f4bbee879a28dfeb6586f910569c6
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-22 22:16:28 +0100

    finished joinnewsletter

--- src/components/static/JoinNewsletter.tsx
+++ src/components/static/JoinNewsletter.tsx
@@ -2,22 +2,25 @@
 
 const JoinNewsletter = () => {
   return (
-    <section className='bg-brandWhite flex justify-center items-center h-fit min-h-[165px] max-h-[205px]'>
-      <div className='inner-container h-[80%] grid grid-flow-col grid-cols-3'>
+    <section className='bg-brandWhite py-5 flex justify-center items-center h-fit min-h-[165px]'>
+      <div className='inner-container h-[80%] grid grid-rows-[min-content,0,min-content] max-xltablet:gap-[clamp(15px,4vw,25px)] xltablet:grid-cols-3'>
         <div className='grid gap-2'>
           <header>
-            <h2 className='text-2xl font-bold'>Join our newsletter</h2>
+            <h2 className='text-[clamp(18px,2vw,24px)] font-bold'>
+              Join our newsletter
+            </h2>
           </header>
-          <p className='w-[30ch]'>
+          <p className='max-w-[30ch]'>
             Get all the latest TNA news delivered to your inbox.
           </p>
         </div>
+
         <div></div>
+
         <div className='h-fit'>
           <form
             action=''
-            className='w-full justify-self-end grid grid-cols-3 gap-3'
-          >
+            className='w-full justify-self-end grid grid-cols-3 gap-3'>
             <input
               type='text'
               name='email'
