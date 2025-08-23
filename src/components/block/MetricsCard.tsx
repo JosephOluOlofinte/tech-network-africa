44ea7d4dad92f449a172d3e2c380cc21fd7e9cda
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-24 00:03:02 +0100

    Continued the about pahe

--- src/components/block/MetricsCard.tsx
+++ src/components/block/MetricsCard.tsx
@@ -8,14 +8,15 @@ type Props = {
 
 const MetricsCard: React.FC<Props> = ({ figure, title, desc }) => {
   return (
-    <div className='backdrop-blur-md bg-[#8B8B8B]/10 border border-brandWhite/30 text-center py-[32px] px-[24px] flex flex-col gap-5'>
-      <p className='text-[60px] leading-[72px] font-inter font-semibold text-white'>
+    <div className='max-w-[345px] backdrop-blur-md bg-[#8B8B8B]/10 border border-brandWhite/30 text-center py-[32px] px-[24px] flex flex-col gap-3 laptop:gap-5 items-center'>
+      <p className='text-3xl tablet:text-4xl desktop:text-5xl leading-[72px] font-inter font-semibold text-white'>
         {figure}
       </p>
-      <p className='text-[18px] leading-[28px] font-inter font-semibold text-white'>
+      <p
+        className='text-base laptop:text-lg font-inter font-semibold text-white'>
         {title}
       </p>
-      <p className='text-[16px] leading-[24px] font-inter text-white w-[30ch]'>
+      <p className='text-sm laptop:text-base leading-[24px] font-inter text-white w-[30ch]'>
         {desc}
       </p>
     </div>
--- src/pages/about/AboutHero.tsx
+++ src/pages/about/AboutHero.tsx
@@ -7,13 +7,13 @@ import MetricsCard from '../../components/block/MetricsCard';
 const AboutHero = () => {
   return (
     <Fragment>
-      <main className='bg-brandWhite h-screen max-h-[850px] flex justify-center items-center'>
-        <div className='inner-container h-[90%] flex flex-col'>
+      <main className='bg-brandWhite h-fit  flex justify-center items-center'>
+        <div className='inner-container py-12 flex flex-col'>
           <Header />
 
-          <div className='w-full h-full flex flex-col justify-between pt-[70px] pb-[30px]'>
-            <div className='text-center'>
-              <h1 className='text-6xl leading-[72px] text-brandDeepGray100 font-medium '>
+          <div className='w-full h-full flex flex-col justify-between gap-20 pt-[70px] pb-[30px]'>
+            <div className='text-center grid h-fit gap-5'>
+              <h1 className='text-[35px] tablet:text-[45px] xllaptop:text-[55px] text-brandDeepGray100 font-medium'>
                 TECH NETWORK AFRICA
               </h1>
               <p className='text-brandDeepGray2 text-2xl pt-2'>
@@ -22,7 +22,7 @@ const AboutHero = () => {
               </p>
             </div>
 
-            <div className='bg-metrics p-[64px] h-[360px] w-full max-w-[1220px] grid grid-cols-[repeat(3,_auto)] gap-[32px]'>
+            <div className='bg-metrics p-[30px] tablet:p-[40px] xllaptop:p-[64px] w-full max-w-[1220px] grid laptop:grid-cols-[repeat(3,_auto)] gap-[32px] justify-center'>
               <MetricsCard
                 figure='1K+'
                 title='Built apartments'
