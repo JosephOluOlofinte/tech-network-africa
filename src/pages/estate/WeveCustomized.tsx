59a9e6e4d061e03baf69bd3af3b401ceba48d9cf
Joseph Olu <josepholuolofinte@gmail.com>
2025-08-30 08:56:11 +0100

    working on product list sections in WeveCustomized

--- src/pages/estate/WeveCustomized.tsx
+++ src/pages/estate/WeveCustomized.tsx
@@ -2,21 +2,23 @@
 
 import { NavLink } from 'react-router-dom';
 import houseImg from '../../assets/images/apartments.png';
+import heartIcon from '../../assets/img_icons/heart-icon.png'
+// import starIcon from '../../assets/img_icons/star-icon.png'
 
 const WeveCustomized = () => {
   return (
     <section className='bg-brandWhite flex justify-center'>
       <div className='inner-container'>
-        <div className='py-14 grid grid-cols-2 items-center'>
-          <h2 className='max-w-[20ch] font-medium'>
+        <div className='py-14 grid xltablet:grid-cols-2 items-center justify-center gap-6'>
+          <h2 className='max-w-[30ch] xltablet:max-w-[20ch] font-medium max-xltablet:text-center'>
             We've customized our apartments to ease living and work experience
             for you
           </h2>
-          <div className='bg-brandBlue5 p-5 rounded-lg'>
+          <div className='bg-brandBlue5 p-8 rounded-lg'>
             <h3 className='font-bold text-brandBlue1 mb-3'>
               Rent, move in, work and Enjoy.
             </h3>
-            <p className='font-medium text-brandDeepGray2'>
+            <p className='font-medium text-brandDeepGray2 max-w-[55ch] xltablet:max-w-[35ch]'>
               You don’t have to worry about how to furnish your apartment or how
               to set up your workstation, we have it all in place. Just move in
               and experience comfort with productivity
@@ -26,35 +28,30 @@ const WeveCustomized = () => {
 
         <div className='w-full py-14 flex max-tablet:flex-col-reverse justify-center items-center gap-5 flex-wrap'>
           {/* THis is where I left. trying to setup perfect responsiveness */}
-          <div className='relative'>
-            <img
-              src={houseImg}
-              alt=''
-              className='h-[200px] w-[265px] rounded-xl'
-            />
-            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs font-medium absolute top-8 left-4'>
+          <div className='relative border border-brandDeepGray4 rounded-xl overflow-hidden'>
+            <img src={houseImg} alt='' className='h-[200px] w-[265px]' />
+            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs text-brandGreen3 font-medium absolute top-8 left-4'>
+              New
+            </span>
+            <div className='p-3 grid gap-3'>
+              <div className='flex justify-between items-center'>
+                <p className='text-xl font-bold'>A room self contain</p>
+                <img src={heartIcon} alt='' className='size-6'/>
+              </div>
+              <div></div>
+            </div>
+          </div>
+
+          <div className='relative border border-brandDeepGray4 rounded-xl overflow-hidden'>
+            <img src={houseImg} alt='' className='h-[200px] w-[265px]' />
+            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs text-brandGreen3 font-medium absolute top-8 left-4'>
               New
             </span>
           </div>
 
-          <div className='relative'>
-            <img
-              src={houseImg}
-              alt=''
-              className='h-[200px] w-[265px] rounded-xl'
-            />
-            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs font-medium absolute top-8 left-4'>
-              New
-            </span>
-          </div>
-
-          <div className='relative'>
-            <img
-              src={houseImg}
-              alt=''
-              className='h-[200px] w-[265px] rounded-xl'
-            />
-            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs font-medium absolute top-8 left-4'>
+          <div className='relative border border-brandDeepGray4 rounded-xl overflow-hidden'>
+            <img src={houseImg} alt='' className='h-[200px] w-[265px]' />
+            <span className='bg-white py-[6px] px-3 rounded-3xl text-xs text-brandGreen3 font-medium absolute top-8 left-4'>
               New
             </span>
           </div>
